88b2fd285e4862131034e75d697ac5e8a394288a
Chukwuma <chukwumaukaha@gmail.com>
2022-10-18 19:01:25 -0700

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
@@ -22,6 +25,24 @@ int itox(unsigned int, char);
 int print_ui(unsigned int);
 
 int ctox(char, va_list);
+
+int print_octal(unsigned int n);
+
+int print_unsig(unsigned int n);
+
+int print_hexaup(unsigned int n);
+
+int print_hexalow(unsigned int n);
+
+int print_bi(unsigned int n);
+
+int _printstring(char *s);
+
+int rot13(char *point);
+
+int print_rev(char *);
+
+int print_hl(uintptr_t n);
 
 
 #endif /*MAIN_H*/
